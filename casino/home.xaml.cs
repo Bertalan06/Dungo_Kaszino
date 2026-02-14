ba251ba011aad7efe6f1f0eba7772171d3e7aca6
MateREC <materejtenyi@gmail.com>
2026-02-14 12:36:42 +0100

    Biztonsági mentés: home.xaml.cs módosítások, feltöltés, kifizetés

--- casino/home.xaml.cs
+++ casino/home.xaml.cs
@@ -18,15 +18,98 @@ namespace casino
     /// <summary>
     /// Interaction logic for home.xaml
     /// </summary>
+    
+
+
     public partial class home : Page
     {
+
+        public static decimal Egyenleg = 50000;
+
         public home()
         {
             InitializeComponent();
+            FrissEgyenleg();
         }
         private void blackjack_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new blackjack());
         }
+
+        private void chickenroad_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new chicken());
+        }
+
+        private void dice_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new dice());
+        }
+
+        private void hilo_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new hilo());
+        }
+
+        private void lezeus_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new lezeus());
+        }
+
+        private void mines_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new mines());
+        }
+
+        private void feltoltes_Click(object sender, RoutedEventArgs e)
+        {
+            feltoltesablak feltolt = new feltoltesablak();
+            feltolt.Owner = Window.GetWindow(this);
+
+            if(feltolt.ShowDialog() == true)
+            {
+                Egyenleg += feltolt.FeltoltottOsszeg;
+                FrissEgyenleg();
+            }
+        }
+
+        private void kifizetes_Click(object sender, RoutedEventArgs e)
+        {
+            kifizetes kifizet = new kifizetes();
+            kifizet.Owner = Window.GetWindow(this);
+
+            if (kifizet.ShowDialog() == true)
+            {
+                if (kifizet.KivettOsszeg > Egyenleg)
+                {
+                    MessageBox.Show("Nincs elég egyenleg a kifizetéshez!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+
+                Egyenleg -= kifizet.KivettOsszeg;
+                FrissEgyenleg();
+            }
+        }
+
+        private void FrissEgyenleg()
+        {
+            egyenlegertek.Text = $"{Egyenleg:N0} Ft";
+
+            if(Egyenleg < 0)
+            {
+                egyenlegertek.Foreground = new SolidColorBrush(Colors.Red);
+            }
+            else if(Egyenleg <= 10000)
+            {
+                egyenlegertek.Foreground = new SolidColorBrush(Colors.Orange);
+            }
+            else
+            {
+                egyenlegertek.Foreground = new SolidColorBrush(Colors.Green);
+            }
+
+            idoTB.Text = "Legutóbbi frissítés: " + DateTime.Now.ToString("HH:mm");
+        }
+
     }
 }
---
+++ casino/kifizetes.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace casino
+{
+    /// <summary>
+    /// Interaction logic for kifizetes.xaml
+    /// </summary>
+    public partial class kifizetes : Window
+    {
+        public decimal KivettOsszeg { get; private set; } = 0;
+
+        public kifizetes()
+        {
+            InitializeComponent();
+        }
+
+        private void Ok2_Click(object sender, RoutedEventArgs e)
+        {
+            if (decimal.TryParse(osszTB.Text, out decimal osszeg) && osszeg > 0)
+            {
+                KivettOsszeg = osszeg;
+                this.DialogResult = true;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Kérlek, adj meg egy pozitív számot!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
