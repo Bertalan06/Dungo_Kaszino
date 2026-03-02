f8348a301639e026821cad76497fc4e29975d55b
rubenszabo <szabo.rube@hbsz.edu.hu>
2026-03-02 21:12:04 +0100

    csatachip

---
+++ casino/profile.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace casino
+{
+    /// <summary>
+    /// Interaction logic for profile.xaml
+    /// </summary>
+    public partial class profile : Page
+    {
+        public profile()
+        {
+            InitializeComponent();
+        }
+        private void vissza_Click(object sender, RoutedEventArgs e)
+        {
+            if (NavigationService != null && NavigationService.CanGoBack)
+            {
+                NavigationService.GoBack();
+            }
+        }
+    }
+}
